cf4a6c5ad961d84eaa167cac26f2f33bc1ab28bf
unknown <andrew.ning.cs@gmail.com>
2020-04-30 11:50:40 -0400

    Fixing some error messages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Home from './Home';
+import { Switch, Route, Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/create"/>
+        <Route path="/join"/>
+        <Route path="/singleplayer"/>
+      </Switch>
+    </Router>
   );
 }
 
