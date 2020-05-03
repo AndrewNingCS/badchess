5007b0062082b2ac2b6feda201f60142101fe7d9
Eric Lin <c84lin@uwaterloo.ca>
2020-05-03 15:09:25 -0400

    added pieces and can click on board

--- src/ChessBoard.js
+++ src/ChessBoard.js
@@ -1,60 +1,194 @@
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import React from 'react';
-import CardContent from '@material-ui/core/CardContent';
+
+import blackKing from './imgs/black_king.png'; // http://clipart-library.com/clipart/pcqrGKzLi.htm
+import whiteKing from './imgs/white_king.png';
+import blackQueen from './imgs/black_queen.png';
+import whiteQueen from './imgs/white_queen.png';
+import blackBishop from './imgs/black_bishop.png';
+import whiteBishop from './imgs/white_bishop.png';
+import blackHorse from './imgs/black_horse.png';
+import whiteHorse from './imgs/white_horse.png';
+import blackRook from './imgs/black_rook.png';
+import whiteRook from './imgs/white_rook.png';
+import blackPawn from './imgs/black_pawn.png';
+import whitePawn from './imgs/white_pawn.png';
 
 
-var num_rows = 8;
-var dark = "#1D2D44";
-var light = "#F0EBD8";
+const numRows = 8;
+const dark = "#F1AD79";
+const light = "#F0EBD8";
+const blockWidth = "100px";
+const blockHeight = "100px";
 
-function Block(coordinate){
-    var color;
-    if (coordinate.charCodeAt(0) % 2 === 0){
-        if (parseInt(coordinate[1]) % 2 === 0){
-            color = dark;
-        } else {
-            color = light;
+
+class Block extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            x: this.props.coordinate[0],
+            y: this.props.coordinate[1],
+            image: "",
         }
-    } else {
-        if (parseInt(coordinate[1]) % 2 === 0){
-            color = light;
-        } else {
-            color = dark;
+
+        let piece = this.props.boardState[this.state.y][this.state.x][0];
+        let isWhite = this.props.boardState[this.state.y][this.state.x][1];
+        let image = "";
+        if (piece === "K") {
+            image = isWhite ? whiteKing : blackKing;
+        } else if (piece === "Q") {
+            image = isWhite ? whiteQueen : blackQueen;
+        } else if (piece === "B") {
+            image = isWhite ? whiteBishop : blackBishop;
+        } else if (piece === "H") {
+            image = isWhite ? whiteHorse : blackHorse;
+        } else if (piece === "R") {
+            image = isWhite ? whiteRook : blackRook;
+        } else if (piece === "P") {
+            image = isWhite ? whitePawn : blackPawn;
         }
+
+        this.state.piece = piece;
+        this.state.isWhite = isWhite;
+        this.state.image = image;
+
+        let selected = this.props.selected;
+        if (selected && selected[0] === this.state.x && selected[1] === this.state.y) {
+            this.state.isSelected = true;
+        } else {
+            this.state.isSelected = false;
+        }
+
+        this.handleClick = this.handleClick.bind(this);
     }
-    return(
-        <Card key={coordinate} style={{width: "100px", height: "100px", backgroundColor: color}}>
-            <CardContent/>
-        </Card>
-    )
+
+    handleClick() {
+        this.setState({ isSelected: !this.state.isSelected })
+        this.props.onCustomClick(this.state);
+    }
+
+    render() {
+        let focus = this.state.isSelected ? "2px solid #3E5C76" : "";
+        if (this.state.image === "") {
+            return (
+                <CardActionArea
+                    square
+                    key={this.state.x}
+                    style={{
+                        width: blockWidth,
+                        height: blockHeight,
+                        backgroundColor: this.props.colour,
+                        border: focus
+                    }}
+                    onClick={() => this.handleClick()}
+                    disableRipple
+                />
+            )
+        }
+        return (
+            <Card
+                square
+                key={this.state.x}
+                onClick={() => this.handleClick()}
+            >
+                <CardActionArea
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignContent: "center",
+                        width: blockWidth,
+                        height: blockHeight,
+                        backgroundColor: this.props.colour,
+                        border: focus
+                    }}
+                    disableRipple
+                >
+                    <img
+                        style={{
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                            width: "80%",
+                            height: "80%"
+                        }}
+                        src={this.state.image}
+                        alt={this.state.piece}
+                    />
+                </CardActionArea>
+            </Card>
+        )
+    }
 }
 
-function Row(key){
-    var row = [];
-    for (var i = 0; i < num_rows; i++){
-        row.push(Block(key + (i + 1)));
+class ChessBoard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false
+        }
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    return(
-        <Grid key={key} container={true}>
-            {row}
-        </Grid>
-    )
-}
+    handleClick(blockState) {
+        let x = blockState.x;
+        let y = blockState.y;
+        let piece = blockState.piece;
+        let isWhite = blockState.isWhite;
+        console.log(`${x}, ${y}, ${piece}, ${isWhite}`);
+    }
 
-function ChessBoard(){
-    var chess_board = [];
-    var start_code = 'A'.charCodeAt(0);
-    for (var i = 0; i < num_rows; i++){
-        var row = String.fromCharCode(start_code + i);
-        chess_board.push(Row(row));
-    }    
-    return(
-        <Grid container={true}>
-            {chess_board}
-        </Grid>
-    )
+    componentDidMount() {
+        fetch("https://badchess-server.herokuapp.com/create_single_player_game")
+            .then(res => res.json())
+            .then(result => this.setState({ gameState: result }))
+            .then(() => {
+                let board = [];
+                for (let y = 0; y < numRows; y++) {
+                    let row = [];
+                    let parity = y % 2 === 0 ? 0 : 1;
+                    for (let x = 0; x < numRows; x++) {
+                        let blockColour = x % 2 === parity ? light : dark;
+                        row.push(
+                            <Block
+                                key={x.toString() + y.toString()}
+                                coordinate={[x, y]}
+                                colour={blockColour}
+                                boardState={this.state.gameState.board}
+                                onCustomClick={this.handleClick}
+                                selected={this.state.selected}
+                            />
+                        );
+                    }
+                    board.push(row);
+                }
+                this.setState({ board: board });
+                this.setState({ loaded: true });
+            })
+    }
+
+    render() {
+        if (this.state.loaded) {
+            return(
+                <Grid container={true}>
+                    {this.state.board.map((x, i) => (
+                        <Grid container key={i}>
+                            {x}
+                        </Grid>
+                    ))}
+                </Grid>
+            )
+        } else {
+            return (
+                <div>
+                    Loading from Server
+                </div>
+            )
+        }
+    }
 }
 
 
